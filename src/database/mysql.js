75636ca0950674a54dbb1434436d2757dc70fac8
Hiick <alexandre@cocoworker.com>
2020-06-16 10:08:00 +0200

    Update: Try to handle MySQL disconneciton

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -4,20 +4,33 @@ const mySql = require('mysql');
 let connection = null;
 
 function query(queryString, callback){
-    connection = mySql.createConnection({
-        host: process.env.HOST,
-        port: process.env.MYSQL_PORT,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE
-    })
-    /*connection = mySql.createConnection({
-        host: 'localhost',
-        port: '8889',
-        user: 'root',
-        password: 'root',
-        database: 'DBTest'
-    })*/
+    function handleDisconnect() {
+        connection = mySql.createConnection({
+            host: process.env.HOST,
+            port: process.env.MYSQL_PORT,
+            user: process.env.USER,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE
+        })
+
+        connection.connect(function(err) {
+            if(err) {
+                console.log('Error when connecting to db:', err);
+                setTimeout(handleDisconnect, 2000);
+            }
+        });
+
+        connection.on('error', function(err) {
+            console.log('db error', err);
+            if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+                handleDisconnect();
+            } else {
+                throw err;
+            }
+        });
+    }
+
+    handleDisconnect();
 
     connection.query(queryString, function(error, results, fields){
         console.log('mySql: query: error is: ', error, ' and results are: ', results);
